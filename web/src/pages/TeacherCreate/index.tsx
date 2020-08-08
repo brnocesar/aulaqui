6bc067480eeaa8236867a254db08ca30dee7d01a
Bruno Cesar <bruunoocesar@gmail.com>
2020-08-07 21:56:30 -0300

    Implementa formulário e novos componentes

--- web/src/pages/TeacherCreate/index.tsx
+++ web/src/pages/TeacherCreate/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RiErrorWarningLine } from 'react-icons/ri';
 import PageHeader from '../../components/PageHeader';
 import FormField from '../../components/FormField';
+import TextArea from '../../components/TextArea';
+import Select from '../../components/Select';
 import './styles.css';
 
 
 function TeacherCreate() {
+    // nao apresentar dias selecionados
+    const availableOptions = [
+        { value: '0', label: 'Domingo' },
+        { value: '1', label: 'Segunda-feira' },
+        { value: '2', label: 'Terça-feira' },
+        { value: '3', label: 'Quarta-feira' },
+        { value: '4', label: 'Quinta-feira' },
+        { value: '5', label: 'Sexta-feira' },
+        { value: '6', label: 'Sábado' },
+    ];
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day: '', from: '', to: '' },
+    ]);
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: '', from: '', to: '' }
+        ]);
+    }
     
     return (
         <div id="page-teacher-create" className="container">
@@ -20,12 +42,43 @@ function TeacherCreate() {
                     <FormField name="name" label="Nomo completo" />
                     <FormField name="avatar" label="Foto" />
                     <FormField name="whatsapp" label="WhatsApp" />
+                    <TextArea name="bio" label="Biografia" /> {/* aumentar o tamanho conforme o texto ocupa espaço */}
                 </fieldset>
                 
                 <fieldset>
                     <legend>Sobre a aula</legend>
-                    <FormField name="subject" label="Matéria" />
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                        ]}
+                    />
                     <FormField name="cost" label="Valor da hora/aula" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + Novo horário
+                        </button>
+                        {/* botao para limpar os campos adicionados */}
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem => (
+                        <div key={scheduleItem.week_day} className="schedule-item">
+                            <Select
+                                name="week_day"
+                                label="Dia da semana"
+                                options={availableOptions}
+                            />
+                            <FormField name="from" label="Das" type="time" />
+                            <FormField name="to" label="Até" type="time" />
+                        </div>
+                    ))}
                 </fieldset>
                 
                 <footer>
--- web/src/pages/TeacherIndex/index.tsx
+++ web/src/pages/TeacherIndex/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 import FormField from '../../components/FormField';
 import TeacherItem from '../../components/TeacherItem';
 import './styles.css';
@@ -11,8 +12,28 @@ function TeacherIndex() {
         <div id="page-teacher-index" className="container">
             <PageHeader title="Tenha aula com os melhores professores sem sair de casa">
                 <form id="search-teachers">
-                    <FormField name="subject" label="Matéria" />
-                    <FormField name="week-day" label="Dia da semana" />
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                        ]}
+                    />
+                    <Select
+                        name="week_day"
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
                     <FormField type="time" name="time" label="Horário" />
                 </form>
             </PageHeader>
